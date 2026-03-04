0194bed27df4505eeda1aac84ff8da344662b818
PC <tuneymitch@gmail.com>
2026-03-04 19:36:50 +0100

    drg

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,12 @@ import adapter from '@sveltejs/adapter-netlify';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter(),
+		version: {
+			pollInterval: 0
+		}
+	},
 
 	vite: {
 		ssr: {
