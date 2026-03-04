7addbb44edfc9a40f168a50e9dedc46e9f6bb35e
PC <tuneymitch@gmail.com>
2026-03-04 17:33:21 +0100

    drg

--- src/lib/gsap.ts
+++ src/lib/gsap.ts
@@ -1,5 +1,5 @@
 import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 // import { Draggable } from 'gsap/dist/Draggable';
 
 // Register plugins globally
